e48bc7ff436d7d23cac27541ceede95fd1589686
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-03-23 17:15:01 +0300

    action

--- FluffyAss/Person.java
+++ FluffyAss/Person.java
@@ -1,5 +1,39 @@
 package FluffyAss;
 
 public class Person {
+    private String sex;
+    private String name;
     
+    public Person(String sex, String name) {
+        this.sex = sex;
+        this.name = name;
+    }
+    
+    public String getSex() {
+        return sex;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String pet() {
+        return this.sex + " " + this.name + " гладит котика.";
+    }
+    
+    public String agression() {
+        return this.name + " ругаекется и кричит на кота.";
+    }
+    
+    public String feedCat() {
+        return this.name + " накладывает котику еду.";
+    }
 }
