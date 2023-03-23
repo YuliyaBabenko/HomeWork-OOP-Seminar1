f3d24c2b92a36620100b4ad6ee6ea6cafd74e4f4
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-03-23 17:19:06 +0300

    final

--- FluffyAss/Main.java
+++ FluffyAss/Main.java
@@ -1,5 +1,15 @@
 package FluffyAss;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Cat cat = new Cat("вислоушка", "Леопольд");
+        Person man = new Person("Мужчина", "Иннокентий");
+        
+        System.out.println(man.pet());
+        System.out.println(cat.voice());
+        System.out.println(man.agression());
+        System.out.println(cat.agression());
+        System.out.println(cat.askForFood());
+        System.out.println(man.feed());
+    }
 }
