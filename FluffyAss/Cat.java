7bb3306d54462bf98dce79c66da631544a97deb7
YuliyaB <114241910+YuliyaBabenko@users.noreply.github.com>
2023-03-23 17:12:40 +0300

    final

--- FluffyAss/Cat.java
+++ FluffyAss/Cat.java
@@ -26,7 +26,7 @@ public class Cat {
     }
     
     public String voice() {
-        return this.model + " " + this.name + " урчит"
+        return this.model + " " + this.name + " урчит";
     }
     
     public String agression() {
